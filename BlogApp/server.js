f1632c097df8faea760e08afa3b115b51d6d02fe
RagniNagpal <ragninagpal@gmail.com>
2025-11-14 21:37:17 +0530

    commit

--- BlogApp/server.js
+++ BlogApp/server.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const mongoose=require("mongoose");
 const app = express();
 
 app.use(express.json());
+const mongoose = require("mongoose");
 
-const users = []; // ✅ define users array
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+module.exports = mongoose.model("User", userSchema);
+async function dbConnect(){
+  try{
+    await mongoose.connect("mongodb://localhost:27017/blogDb");
+    console.log("Db connected successfully");
+  }catch(error){
+    console.log("error");
+    console.log(error)
+  }
+}
+
+const users = []; 
 
 app.post("/users", (req, res) => {
   const { name, password, email } = req.body;
@@ -70,28 +89,24 @@ app.get("/users/:id",(req,res)=>{
   }
 })
 
-app.patch("/users/:id", async (req, res) => {
+app.patch("/users/:id", (req, res) => {
   try {
-    const id = req.params.id;
-    const { name, password, email } = req.body;
+    const id = parseInt(req.params.id);
+    const userIndex = users.findIndex(u => u.id === id);
 
-    const updatedUser = await User.findByIdAndUpdate(
-      id,
-      { name, password, email },
-      { new: true } // returns updated document
-    );
-
-    if (!updatedUser) {
+    if (userIndex === -1) {
       return res.status(404).json({
         success: false,
-        message: "User not found",
+        message: "User not found"
       });
     }
+
+    users[userIndex] = { ...users[userIndex], ...req.body };
 
     return res.status(200).json({
       success: true,
       message: "User updated successfully",
-      data: updatedUser,
+      data: users[userIndex],
     });
 
   } catch (err) {
@@ -103,18 +118,20 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
-app.delete("/users/:id", async (req, res) => {
+
+app.delete("/users/:id", (req, res) => {
   try {
-    const id = req.params.id;
+    const id = parseInt(req.params.id);
+    const userIndex = users.findIndex(u => u.id === id);
 
-    const deletedUser = await User.findByIdAndDelete(id);
-
-    if (!deletedUser) {
+    if (userIndex === -1) {
       return res.status(404).json({
         success: false,
         message: "User not found",
       });
     }
+
+    const deletedUser = users.splice(userIndex, 1);
 
     return res.status(200).json({
       success: true,
@@ -132,6 +149,8 @@ app.delete("/users/:id", async (req, res) => {
 });
 
 
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
+  dbConnect()
 });
